c2a8709d6da56e1b32a809233335f45d46371517
Seth Myers <thesethhasarrived@gmail.com>
2021-05-08 18:16:48 -0400

    added forced logout command

---
+++ commands/security/flogout.js
@@ -0,0 +1,29 @@
+const moment = require('moment');
+module.exports = {
+	commands: ['forcelogout', 'flog'],
+	category: 'Security',
+	description: 'Log out a user forcefully',
+    minArgs: 1,
+    maxArgs: 1,
+    expectedArgs: '<@user>',
+	guildOnly: true,
+    ownerOnly: 'true',
+	callback: async ({ message, client, args }) => {
+		const Discord = require('discord.js');
+  const time = moment();
+  const timeFull = time.format('MMMM Do YYYY, h:mm:ss a');
+  const user = message.mentions.members.first() || await message.guild.members.fetch(args[0]).catch(err => {
+ return;
+  });
+  message.delete();
+  if (user) {
+	const embed = new Discord.MessageEmbed()
+	.setColor('#ff0000')
+	.setTitle('Security Alert')
+	.setDescription(`${user} was forcefully logged out ${timeFull}`);
+	client.channels.cache.get('836421516546539561').send(embed);
+	const role = message.member.guild.roles.cache.find(role => role.id === '836432624037134377');
+	user.roles.remove(role);
+  }
+	},
+};
