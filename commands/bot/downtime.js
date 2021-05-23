2067d43ae7e63ec5169fbe5260bc605d13b3dd48
ThatGiantSeth <42821973+ThatGiantSeth@users.noreply.github.com>
2021-05-23 18:46:35 -0400

    1.5.0: added issue and resolve commands

---
+++ commands/bot/downtime.js
@@ -0,0 +1,41 @@
+module.exports = {
+	commands: ['offline', 'down'],
+	category: 'Bot',
+	description: 'Make a downtime message for the bot.',
+	ownerOnly: true,
+	testOnly: true,
+	minArgs: 2,
+	expectedArgs: '<"Maintenance" or "Iincident"> <message>',
+	cooldown: '10s',
+	callback: async ({ message, args, client }) => {
+		const Discord = require('discord.js');
+		const issueType = args[0];
+		args.shift();
+		const issueMessage = args.join(' ');
+		if (issueType === 'Maintenance' || issueType === 'Incident') {
+		const embed = new Discord.MessageEmbed()
+		.setTitle(`${issueType}`)
+		.setColor('#ff0000')
+		.setDescription(`${issueMessage}\n\nSee status page for details here: https://hayasaka.statuspage.io/`)
+		.setFooter('Hayasaka', 'https://i.imgur.com/W1lcK9M.gif');
+		const channel = client.channels.cache.get('840242888935473242');
+		try {
+			const webhooks = await channel.fetchWebhooks();
+			const webhook = webhooks.first();
+
+			await webhook.send('', {
+				username: 'Hayasaka Information',
+				avatarURL: 'https://i.imgur.com/OjYg78u.jpg',
+				embeds: [embed],
+			});
+		}
+		catch (error) {
+			message.reply('There was an error sending the webhook');
+		}
+		message.delete();
+		}
+		else {
+			message.reply('issue type must be Maintenance or Incident');
+		}
+	},
+};
---
+++ commands/bot/resolve.js
@@ -0,0 +1,82 @@
+module.exports = {
+	commands: ['online', 'up'],
+	category: 'Bot',
+	description: 'Make an issue resolved message for the bot.',
+	ownerOnly: true,
+	testOnly: true,
+	minArgs: 2,
+	expectedArgs: '<"Maintenance", "Incident", or "Update"> <message>',
+	cooldown: '10s',
+	callback: async ({ message, args, client }) => {
+		const Discord = require('discord.js');
+		const issueType = args[0];
+		args.shift();
+		const issueMessage = args.join(' ');
+		if (issueType === 'Maintenance') {
+		const embed = new Discord.MessageEmbed()
+		.setTitle('Maintenance Completed')
+		.setColor('#00ff47')
+		.setDescription(`${issueMessage}\n\nFor information about downtimes, visit the status page here: https://hayasaka.statuspage.io/`)
+		.setFooter('Hayasaka', 'https://i.imgur.com/W1lcK9M.gif');
+		const channel = client.channels.cache.get('840242888935473242');
+		try {
+			const webhooks = await channel.fetchWebhooks();
+			const webhook = webhooks.first();
+
+			await webhook.send('', {
+				username: 'Hayasaka Information',
+				avatarURL: 'https://i.imgur.com/OjYg78u.jpg',
+				embeds: [embed],
+			});
+		}
+		catch (error) {
+			message.reply('There was an error sending the webhook');
+		}
+		message.delete();
+		}
+		if (issueType === 'Incident') {
+			const embed = new Discord.MessageEmbed()
+			.setTitle('Incident Resolved')
+			.setColor('#00ff47')
+			.setDescription(`${issueMessage}\n\nFor information about downtimes, visit the status page here: https://hayasaka.statuspage.io/`)
+			.setFooter('Hayasaka', 'https://i.imgur.com/W1lcK9M.gif');
+			const channel = client.channels.cache.get('840242888935473242');
+			try {
+				const webhooks = await channel.fetchWebhooks();
+				const webhook = webhooks.first();
+
+				await webhook.send('', {
+					username: 'Hayasaka Information',
+					avatarURL: 'https://i.imgur.com/OjYg78u.jpg',
+					embeds: [embed],
+				});
+			}
+			catch (error) {
+				message.reply('There was an error sending the webhook');
+			}
+			message.delete();
+		}
+		if (issueType === 'Update') {
+			const embed = new Discord.MessageEmbed()
+			.setTitle('Incident Update')
+			.setColor('#0070ff')
+			.setDescription(`${issueMessage}\n\nFor information about downtimes, visit the status page here: https://hayasaka.statuspage.io/`)
+			.setFooter('Hayasaka', 'https://i.imgur.com/W1lcK9M.gif');
+			const channel = client.channels.cache.get('840242888935473242');
+			try {
+				const webhooks = await channel.fetchWebhooks();
+				const webhook = webhooks.first();
+
+				await webhook.send('', {
+					username: 'Hayasaka Information',
+					avatarURL: 'https://i.imgur.com/OjYg78u.jpg',
+					embeds: [embed],
+				});
+			}
+			catch (error) {
+				message.reply('There was an error sending the webhook');
+			}
+			message.delete();
+		}
+	},
+};
