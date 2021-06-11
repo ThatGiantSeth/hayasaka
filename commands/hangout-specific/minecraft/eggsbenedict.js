169490965e6afa40f1c0e89019001b2c90802424
ThatGiantSeth <42821973+ThatGiantSeth@users.noreply.github.com>
2021-06-10 22:51:39 -0400

    remove launcher references

--- commands/hangout-specific/minecraft/eggsbenedict.js
+++ commands/hangout-specific/minecraft/eggsbenedict.js
@@ -12,7 +12,7 @@ module.exports = {
 			const embed = new Discord.MessageEmbed()
             .setTitle('Full Info View')
             .setColor('#000000')
-            .setDescription('**Rules (you must read these)**:\nhttps://docs.google.com/document/d/16SNqpJ5ZstpP2D77-gN_9xBETJZi002WFZJ-Gji_70k/edit \n\nServer Address/Port: `70.161.60.201:35584`\nGame Version: `1.12.2`\nGamemode: `Survival`\nModded: `Yes`\nForge Version (You don\'t need the same one but it must be equal to or newer): `14.23.5.2847`\n\nA list of mods, versions, and descriptions can be found here:\nhttps://docs.google.com/spreadsheets/d/1Lm7ZVWK37m9Ou5W0xAs1nAUOyvO9Bny5d3MjGtoPNcE/edit \n\nCustom Modpack Installer and Launcher instructions:\nhttps://docs.google.com/document/d/1cWx4sZMcx3pCfcpXpGDJ5GwwZyoQ6UUtkxHXPKX4y7A/edit?usp=sharing \n\nIf you\'d like to install the mods manually:\nhttps://docs.google.com/document/d/1-aPMETJhhBA8rqcj-e2hWzBcdIx8yaZ62QkvkXxtZ4s/edit \n\nExtras:\nDynamic Map Access - http://70.161.60.201:8123/ \nThere\'s a sidebar on the right where you can change the map type and stuff like that.')
+            .setDescription('**Rules (you must read these)**:\nhttps://docs.google.com/document/d/16SNqpJ5ZstpP2D77-gN_9xBETJZi002WFZJ-Gji_70k/edit \n\nServer Address/Port: `70.161.60.201:35584`\nGame Version: `1.12.2`\nGamemode: `Survival`\nModded: `Yes`\nForge Version (You don\'t need the same one but it must be equal to or newer): `14.23.5.2847`\n\nA list of mods, versions, and descriptions can be found here:\nhttps://docs.google.com/spreadsheets/d/1Lm7ZVWK37m9Ou5W0xAs1nAUOyvO9Bny5d3MjGtoPNcE/edit  \n\nDownload links and instructions can be found here:\nhttps://docs.google.com/document/d/1-aPMETJhhBA8rqcj-e2hWzBcdIx8yaZ62QkvkXxtZ4s/edit \n\nExtras:\nDynamic Map Access - http://70.161.60.201:8123/ \nThere\'s a sidebar on the right where you can change the map type and stuff like that.')
             .setFooter('Hayasaka', 'https://i.imgur.com/W1lcK9M.gif');
             message.channel.send(embed);
 		}
@@ -28,7 +28,7 @@ module.exports = {
 			const embed = new Discord.MessageEmbed()
 			.setTitle('Installation Guide')
 			.setColor('#000000')
-			.setDescription('Custom Installer and Launcher instructions:\nhttps://docs.google.com/document/d/1cWx4sZMcx3pCfcpXpGDJ5GwwZyoQ6UUtkxHXPKX4y7A/edit?usp=sharing \n\nIf you\'d like to install the mods manually:\nhttps://docs.google.com/document/d/1-aPMETJhhBA8rqcj-e2hWzBcdIx8yaZ62QkvkXxtZ4s/edit')
+			.setDescription('Download links and instructions can be found here:\nhttps://docs.google.com/document/d/1-aPMETJhhBA8rqcj-e2hWzBcdIx8yaZ62QkvkXxtZ4s/edit')
 			.setFooter('Hayasaka', 'https://i.imgur.com/W1lcK9M.gif');
             message.channel.send(embed);
 		}
